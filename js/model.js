5bcf0b8e1446650d5c688b7bfa8c5e9607011964
Edward Jornes <sethyathet@sethyathets-MacBook-Air.local>
2016-06-11 15:06:22 -0400

    js

---
+++ js/model.js
@@ -0,0 +1,22 @@
+/*===Model for all locations===*/
+
+var myLocations = [
+  {
+    name: 'Prudential Center',
+    lat: 42.34852,
+    lng: -71.08230,
+    address: '800 Boylston St, Boston, MA 02199'
+  },
+  {
+    name: 'Central Library',
+    lat: 42.34942,
+    lng: -71.07883,
+    address: '700 Boylston St, Boston, MA 02116'
+  },
+  {
+    name: 'Boston Common',
+    lat: 42.34813,
+    lng: -71.06776,
+    address: 'Tremont St, Boston, MA 02116'
+  }
+];
