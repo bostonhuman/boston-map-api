aa575473f89151b16e8146a5f10e5ebf23e4abe5
Edward Jornes <sethyathet@sethyathets-MacBook-Air.local>
2016-06-11 23:25:53 -0400

    script

--- js/script.js
+++ js/script.js
@@ -1,3 +1,29 @@
+/*===Model for all locations===*/
+
+var myLocations = [
+  {
+    name: 'Prudential Center',
+    lat: 42.34852,
+    lng: -71.08230,
+    address: '800 Boylston St, Boston, MA 02199',
+    neighborhood: 'Boston, MA'
+  },
+  {
+    name: 'Central Library',
+    lat: 42.34942,
+    lng: -71.07883,
+    address: '700 Boylston St, Boston, MA 02116',
+    neighborhood: 'Boston, MA'
+  },
+  {
+    name: 'Boston Common',
+    lat: 42.34813,
+    lng: -71.06776,
+    address: 'Tremont St, Boston, MA 02116',
+    neighborhood: 'Boston, MA'
+  }
+];
+
 // Initialize the map
 var map;
 
