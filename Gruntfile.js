3149ef16b405b8d388799ed33e706d16e500ddaa
Edward Jornes <sethyathet@sethyathets-MacBook-Air.local>
2016-06-12 22:04:07 -0400

    Gruntfile.js

---
+++ Gruntfile.js
@@ -0,0 +1,26 @@
+module.exports = function(grunt) {
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	
+	grunt.initConfig ({   	  
+   		uglify: {
+			my_target: {
+				files: {
+					'js/script.min.js' : 'js/script.js',
+					'js/mobile.min.js': 'js/mobile.js'
+				}
+			}
+		},
+		cssmin: {
+			target: {
+				files: {
+					'css/style.min.css': 'css/style.css'
+    			}
+  			}
+		}
+	});	
+	grunt.registerTask('default', [
+		'cssmin',
+		'uglify'
+	]);
+};
