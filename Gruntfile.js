27cafaf6a73f1817e8494b377c46db9ea42b00ac
Edward Jornes <sethyathet@sethyathets-MacBook-Air.local>
2016-06-12 23:22:38 -0400

    gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,7 @@
 module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-concat');
 	
 	grunt.initConfig ({   	  
    		uglify: {
@@ -17,10 +18,20 @@ module.exports = function(grunt) {
 					'css/style.min.css': 'css/style.css'
     			}
   			}
-		}
+		},
+		concat: {
+			options: {
+			separator: ';',
+    	},
+		dist: {
+			src: ['js/lib/jquery-2.2.4.min.js', 'js/lib/bootstrap.min.js', 'js/lib/knockoutjs-v3.4.0.js', 'oauth-signature/dist/oauth-signature.min.js', 'js/script.min.js', 'js/mobile.min.js'],
+			dest: 'js/built-concat.min.js',
+    		},
+  		},
 	});	
 	grunt.registerTask('default', [
 		'cssmin',
-		'uglify'
+		'uglify',
+		'concat'
 	]);
 };
