8bd0cd4d9a9c013432461a368be3beca1f76350f
Edward Jornes <sethyathet@sethyathets-MacBook-Air.local>
2016-06-15 20:55:58 -0400

    gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,19 +19,9 @@ module.exports = function(grunt) {
     			}
   			}
 		},
-		concat: {
-			options: {
-			separator: ';',
-    	},
-		dist: {
-			src: ['js/lib/jquery-2.2.4.min.js', 'js/lib/bootstrap.min.js', 'js/lib/knockoutjs-v3.4.0.js', 'oauth-signature/dist/oauth-signature.min.js', 'js/script.min.js', 'js/mobile.min.js'],
-			dest: 'js/built-concat.min.js',
-    		},
-  		},
 	});	
 	grunt.registerTask('default', [
 		'cssmin',
-		'uglify',
-		'concat'
+		'uglify'
 	]);
 };
